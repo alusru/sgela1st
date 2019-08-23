f78b1f7b4f428d8e36742e3b45b3c03211385114
Mbuso <collen.nkabinde@gmail.com>
2019-08-23 15:32:11 +0200

    content changes

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -1,87 +1,194 @@
 export const configuration = {
   header: {
-  heading: 'motivational speaking and youth development organization aimed',
-  introcopy: 'at helping and encouraging learners to make their schooling a priority.'
-},
-whatwedo: {
-  heading: 'What we Do',
-  introcopy: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dignissimos \n Lorem ipsum dolor sit amet, consectetur',
-  services: [
-    // tslint:disable-next-line:max-line-length
-            {Icon: 'book', Title: 'Morning Motivations', Description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.', Time: '0.2s'},
-            // tslint:disable-next-line:max-line-length
-            {Icon: 'pencil', Title: 'Matric motivation 101', Description: 'Loreasdasctetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.', Time: '0.8s'},
-            // tslint:disable-next-line:max-line-length
-            {Icon: 'star', Title: 'Seminars', Description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.', Time: '1.2s'},
-			 // tslint:disable-next-line:max-line-length
-            {Icon: 'book', Title: 'Confidence and Warm', Description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.', Time: '0.2s'},
-            // tslint:disable-next-line:max-line-length
-            {Icon: 'pencil', Title: 'Sgela 1st Top Ten', Description: 'Loreasdasctetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.', Time: '0.8s'},
-            // tslint:disable-next-line:max-line-length
-            {Icon: 'star', Title: 'Sgela 1st Tv', Description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.', Time: '1.2s'}
-          ]
-},
-team: {
+    heading: "motivational speaking and youth development organization aimed",
+    introcopy:
+      "at helping and encouraging learners to make their schooling a priority."
+  },
+  whatwedo: {
+    heading: "What we Do",
+    introcopy:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dignissimos \n Lorem ipsum dolor sit amet, consectetur",
+    services: [
+      // tslint:disable-next-line:max-line-length
+      {
+        Icon: "book",
+        Title: "Morning Motivations",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.",
+        Time: "0.2s"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        Icon: "pencil",
+        Title: "Matric motivation 101",
+        Description:
+          "Loreasdasctetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.",
+        Time: "0.8s"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        Icon: "star",
+        Title: "Seminars",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.",
+        Time: "1.2s"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        Icon: "book",
+        Title: "Confidence and Warm",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.",
+        Time: "0.2s"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        Icon: "pencil",
+        Title: "Sgela 1st Top Ten",
+        Description:
+          "Loreasdasctetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.",
+        Time: "0.8s"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        Icon: "star",
+        Title: "Sgela 1st Tv",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscingasdasdsibh euismod tincidunt ut.",
+        Time: "1.2s"
+      }
+    ]
+  },
+  team: {
     teaminfo: [
-       // tslint:disable-next-line:max-line-length
-              {path: 'les.jpeg', teamname: 'Lesego Tsatseng Rantsho', teamtitle: 'Founder & Chairperson', facebook: 'john', twitter: 'john', linkedin: 'john', ta: 'blank'},
-              // tslint:disable-next-line:max-line-length
-              {path: 'team7.jpeg', teamname: 'Nhlakanipho Sindane', teamtitle: 'Secretary', facebook: 'paul', twitter: 'paul', linkedin: 'paul', ta: 'blank'},
-              // tslint:disable-next-line:max-line-length
-              {path: 'team4.jpeg', teamname: 'Christinah Sefote Manamela', teamtitle: 'Economist', facebook: 'emilly', twitter: 'emilly', linkedin: 'emilly', ta: 'blank'},
-              // tslint:disable-next-line:max-line-length
-              {path: 'team6.jpeg', teamname: 'Mbuso Nkabinde', teamtitle: 'Head of IT', facebook: 'mbuso.nkabinde.94', twitter: 'BasicBlac', linkedin: 'in/mbuso-nkabinde-48905790/', ta: 'blank'},
-              // tslint:disable-next-line:max-line-length
-              {path: 'team5.jpeg', teamname: 'Derick Nkosi', teamtitle: '', facebook: 'mbuso.nkabinde.94', twitter: 'BasicBlac', linkedin: 'in/mbuso-nkabinde-48905790/', ta: 'blank'}
-			  // tslint:disable-next-line:max-line-length
-              {path: 'team4.jpg', teamname: 'Andrian Sibeko', teamtitle: 'Head of IT', facebook: 'mbuso.nkabinde.94', twitter: 'BasicBlac', linkedin: 'in/mbuso-nkabinde-48905790/', ta: 'blank'}
+      // tslint:disable-next-line:max-line-length
+      {
+        path: "team2.jpeg",
+        teamname: "Lesego Tsatseng Rantsho",
+        teamtitle: "Founder & Chairperson",
+        facebook: "john",
+        twitter: "john",
+        linkedin: "john",
+        ta: "blank"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        path: "team3.jpeg",
+        teamname: "Nhlakansipho Sindane",
+        teamtitle: "Secretary",
+        facebook: "paul",
+        twitter: "paul",
+        linkedin: "paul",
+        ta: "blank"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        path: "team4.jpeg",
+        teamname: "Christinah Sefote Manamela",
+        teamtitle: "Economist",
+        facebook: "emilly",
+        twitter: "emilly",
+        linkedin: "emilly",
+        ta: "blank"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        path: "team2.jpeg",
+        teamname: "Mbuso Nkabinde",
+        teamtitle: "Head of IT",
+        facebook: "mbuso.nkabinde.94",
+        twitter: "BasicBlac",
+        linkedin: "in/mbuso-nkabinde-48905790/",
+        ta: "blank"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        path: "team1.jpeg",
+        teamname: "Derick Nkosi",
+        teamtitle: "",
+        facebook: "mbuso.nkabinde.94",
+        twitter: "BasicBlac",
+        linkedin: "in/mbuso-nkabinde-48905790/",
+        ta: "blank"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        path: "team2.jpeg",
+        teamname: "Andrian Sibeko",
+        teamtitle: "Head of IT",
+        facebook: "mbuso.nkabinde.94",
+        twitter: "BasicBlac",
+        linkedin: "in/mbuso-nkabinde-48905790/",
+        ta: "blank"
+      }
     ]
   },
   ourproject: {
     projectinfo: [
-       // tslint:disable-next-line:max-line-length
-              {title: 'Motivations', eventname: 'Petit', exp1: 'CEO'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'Top 10', eventname: 'Petit', exp1: 'CEO'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'asd', eventname: 'benoni high ', exp1: 'CEO'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'asd', eventname: 'lesiba', exp1: 'CEO'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'asd', eventname: 'lwazi', exp1: 'CEO'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'as', eventname: 'mabuya', exp1: 'CEO'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'sdf', eventname: 'davey', exp1: 'Head of IT'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'sdf', eventname: 'Mbunde', exp1: 'Head of IT'}
+      // tslint:disable-next-line:max-line-length
+      { title: "Motivations", eventname: "Petit", exp1: "CEO" },
+      // tslint:disable-next-line:max-line-length
+      { title: "Top 10", eventname: "Petit", exp1: "CEO" },
+      // tslint:disable-next-line:max-line-length
+      { title: "asd", eventname: "benoni high ", exp1: "CEO" },
+      // tslint:disable-next-line:max-line-length
+      { title: "asd", eventname: "lesiba", exp1: "CEO" },
+      // tslint:disable-next-line:max-line-length
+      { title: "asd", eventname: "lwazi", exp1: "CEO" },
+      // tslint:disable-next-line:max-line-length
+      { title: "as", eventname: "mabuya", exp1: "CEO" },
+      // tslint:disable-next-line:max-line-length
+      { title: "sdf", eventname: "davey", exp1: "Head of IT" },
+      // tslint:disable-next-line:max-line-length
+      { title: "sdf", eventname: "Mbunde", exp1: "Head of IT" }
     ]
   },
   contact: {
-      heading: 'Contact With us',
-      address: 'Street Address here',
-      phone: '(123456789)',
-      email: 'hello@sgela1st.co.za',
-      socialmedia: 'sgela1st',
-      ta: '_blank'
+    heading: "Contact Wissth us",
+    //link: "?pb=!1m18!1m12!1m3!1d28652.99629991058!2d28.4045924382925!3d-26.143782416671613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e953bb63337110f%3A0x5d7ee1a34ca3b1c1!2sDaveyton%2C+Benoni%2C+1507!5e0!3m2!1sen!2sza!4v1566566002843!5m2!1sen!2sza",
+    address: "Street Address here",
+    phone: "(011) 969 3881",
+    email: "hello@sgela1st.co.za",
+    socialmedia: "sgela1st",
+    ta: "_blank"
   },
   footer: {
-    emoji: 'heart'
+    emoji: "heart"
   },
   feature: {
-    heading: 'Motivations',
-    copy: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dignissimos! <br> Lorem ipsum dolor sit amet, consectetur.',
+    heading: "Motivations",
+    copy:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dignissimos! <br> Lorem ipsum dolor sit amet, consectetur.",
     moti: [
-       // tslint:disable-next-line:max-line-length
-              {title: 'Bootstrap 4 Based', copy: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', icon: 'rocket'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'Fully Responsive', copy: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', icon: 'laptop-phone'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'Parallax Background', copy: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', icon: 'layers'},
-              // tslint:disable-next-line:max-line-length
-              {title: 'Easy to Customize', copy: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', icon: 'cog'}
-              // tslint:disable-next-line:max-line-length
-
+      // tslint:disable-next-line:max-line-length
+      {
+        title: "Bootstrap 4 Based",
+        copy:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        icon: "rocket"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        title: "Fully Responsive",
+        copy:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        icon: "laptop-phone"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        title: "Parallax Backgrossund",
+        copy:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        icon: "layers"
+      },
+      // tslint:disable-next-line:max-line-length
+      {
+        title: "Easy to Customize",
+        copy:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        icon: "cog"
+      }
+      // tslint:disable-next-line:max-line-length
     ]
-  },
+  }
 };
